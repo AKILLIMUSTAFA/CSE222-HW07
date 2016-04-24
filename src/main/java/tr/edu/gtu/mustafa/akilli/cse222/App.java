84ab24106a49c70b63ffc752243ea95fce597906
MUSTAFA AKILLI <makilli@gtu.edu.tr>
2016-04-24 19:37:15 +0300

    First Commit
    I started HW07

---
+++ src/main/java/tr/edu/gtu/mustafa/akilli/cse222/App.java
@@ -0,0 +1,13 @@
+package tr.edu.gtu.mustafa.akilli.cse222;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
