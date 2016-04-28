8e705814f836776c529bcc68df026efcbafcb510
MUSTAFA AKILLI <makilli@gtu.edu.tr>
2016-04-28 19:03:53 +0300

    The End

--- src/main/java/tr/edu/gtu/mustafa/akilli/cse222/TestScenario.java
+++ src/main/java/tr/edu/gtu/mustafa/akilli/cse222/TestScenario.java
@@ -1,5 +1,8 @@
 package tr.edu.gtu.mustafa.akilli.cse222;
-import java.util.Scanner;
+import tr.edu.gtu.mustafa.akilli.cse222.part1.CustomerComparator;
+import tr.edu.gtu.mustafa.akilli.cse222.part1.CustomerPriorityQueue;
+import tr.edu.gtu.mustafa.akilli.cse222.part2.UniversityHashTable;
+
 import java.io.*;
 
 /**
@@ -17,7 +20,10 @@ import java.io.*;
 public class TestScenario
 {
 
-    public static void main( String[] args ) throws FileNotFoundException {
+    public static void main( String[] args ) throws IOException {
+
+        /* print screen */
+        System.out.println("\nTest For Data 1 (Part 1)\n");
 
         /* Make a comparator */
          CustomerComparator myComparator = new CustomerComparator();
@@ -28,15 +34,63 @@ public class TestScenario
         customerPriorityQueue.startTheQueue();
 
         /* print screen */
-        System.out.println("\nTest For Data 1\n");
+        System.out.println("\nTest For Data 1 (Part 1)\n");
 
         /* call printServedCustomers method */
         customerPriorityQueue.printServedCustomers();
+
+        /* print screen */
+        System.out.println("\nTest For Data 2 (Part 1)\n");
 
         /*Make a new Priority Queue for Customer */
         CustomerPriorityQueue customerPriorityQueueTwo = new CustomerPriorityQueue("data2.txt",myComparator);
 
          /* call startTheQueue method for customerPriorityQueueTwo*/
         customerPriorityQueueTwo.startTheQueue();
+
+        /* print screen */
+        System.out.println("\nTest For Data 2 (Part 1)\n");
+
+        /* call printServedCustomers method */
+        customerPriorityQueueTwo.printServedCustomers();
+
+
+        /*Random Number Generator
+        Random randomGenerator = new Random();
+        for (int idx = 1; idx <= 500; ++idx){
+            int randomInt = randomGenerator.nextInt(10000);
+            System.out.println((randomInt+5000));
+        }*/
+
+        /* UniversityHashTable */
+        UniversityHashTable universityHashTable = new UniversityHashTable("data500.txt");
+
+        /* print screen */
+        System.out.println("\nTest For Data 500 (Part 2)\n");
+
+        /* Average Collusion Number  */
+        System.out.println("Average Collusion Number : " + universityHashTable.averageCollisionNumber()+ "\n");
+
+        /* print screen equals */
+        if(universityHashTable.equals(2093,8220) == true)
+            System.out.println("2093,8220 belongs to the same asistant.(expected)");
+
+        else
+            System.out.println("2093,8220 not belongs to the same asistant. (unexpected)");
+
+        /* print screen equals */
+        if(universityHashTable.equals(4589,9430) == true)
+            System.out.println("4589,9430 belongs to the same asistant.(expected)");
+
+        else
+            System.out.println("4589,9430 not belongs to the same asistant. (unexpected)");
+
+        /* print screen equals */
+        if(universityHashTable.equals(1453,6645) == true)
+            System.out.println("1453,6645 belongs to the same asistant.(unexpected)");
+
+        else
+            System.out.println("1453,6645 not belongs to the same asistant. (expected)");
+
     }
 }
